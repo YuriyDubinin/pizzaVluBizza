349ae7710149252d75417d6ec8e6748ed9bc49ee
Yura Dubinin <yuradubinin100@gmail.com>
2022-01-03 14:54:17 +0700

    add rough slider functionality, +slider content

--- js/script.js
+++ js/script.js
@@ -4,3 +4,34 @@ $(document).ready(function () {
         $("body").toggleClass("scroll-lock");
     });
 });
+
+//main-slider
+
+let slides = document.querySelectorAll(".main-slider__item"),
+    btnRight = document.querySelector(".main-slider-btn-right"),
+    btnLeft = document.querySelector(".main-slider-btn-left");
+
+let mainSliderCounter = 0;
+
+btnLeft.addEventListener("click", function () {
+    if (mainSliderCounter === 0) {
+        slides[mainSliderCounter].style.display = "none";
+        mainSliderCounter = slides.length - 1;
+        slides[mainSliderCounter].style.display = "block";
+    } else {
+        slides[mainSliderCounter].style.display = "none";
+        --mainSliderCounter;
+        slides[mainSliderCounter].style.display = "block";
+    }
+});
+btnRight.addEventListener("click", function () {
+    if (mainSliderCounter === slides.length - 1) {
+        slides[mainSliderCounter].style.display = "none";
+        mainSliderCounter = 0;
+        slides[mainSliderCounter].style.display = "block";
+    } else {
+        slides[mainSliderCounter].style.display = "none";
+        ++mainSliderCounter;
+        slides[mainSliderCounter].style.display = "block";
+    }
+});
