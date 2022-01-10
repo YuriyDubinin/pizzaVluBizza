0cef0ed2878935ed89383523f44015ef93d16a71
Yura Dubinin <yuradubinin100@gmail.com>
2022-01-10 22:44:08 +0700

    added a function that switches slides in time if the user has no focused on the slider

--- js/script.js
+++ js/script.js
@@ -5,14 +5,17 @@ $(document).ready(function () {
     });
 });
 
-//main-slider
+/* main-slider */
+//declaration
 let slides = document.querySelectorAll(".main-slider__item"),
     btnNext = document.querySelector(".main-slider-btns__next"),
     btnPrev = document.querySelector(".main-slider-btns__prev");
 
 let slideIndex = 0;
 
-btnNext.addEventListener("click", () => {
+//functions
+//next slide
+function switchesToNextSlide() {
     if (slideIndex === slides.length - 1) {
         slides[slideIndex].classList.remove("show", "fade");
         slides[slideIndex].classList.add("hide");
@@ -26,8 +29,10 @@ btnNext.addEventListener("click", () => {
         slides[slideIndex].classList.remove("hide");
         slides[slideIndex].classList.add("show", "fade");
     }
-});
-btnPrev.addEventListener("click", () => {
+}
+
+//prev slide
+function switchesToPrevSlide() {
     if (slideIndex === 0) {
         slides[slideIndex].classList.remove("show", "fade");
         slides[slideIndex].classList.add("hide");
@@ -41,4 +46,55 @@ btnPrev.addEventListener("click", () => {
         slides[slideIndex].classList.remove("hide");
         slides[slideIndex].classList.add("show", "fade");
     }
+}
+
+//change slides by time
+function switchesSlidesByTime() {
+    /* switches slides in time, stops switching when the user hovers the 
+    mouse over the image or button, restores timer when leaving area */
+    let timerId = setInterval(switchesToNextSlide, 4000);
+
+    const slider = document.querySelector(".main-slider__slides");
+
+    let images = slider.querySelectorAll("img");
+
+    //stops automatic change of slides when howering over the image
+    images.forEach((item) => {
+        item.addEventListener("mouseenter", () => {
+            clearInterval(timerId);
+        });
+    });
+
+    //starts automatic change of slides when the mouse leaving from the image
+    images.forEach((item) => {
+        item.addEventListener("mouseleave", () => {
+            timerId = setInterval(switchesToNextSlide, 4000);
+        });
+    });
+
+    //stops automatic change of slides when howering over the slider buttons
+    [btnNext, btnPrev].forEach((item) => {
+        item.addEventListener("mouseenter", () => {
+            clearInterval(timerId);
+        });
+    });
+
+    //starts automatic change of slides when the mouse leaving from slider buttons
+    [btnNext, btnPrev].forEach((item) => {
+        item.addEventListener("mouseleave", () => {
+            timerId = setInterval(switchesToNextSlide, 4000);
+        });
+    });
+}
+
+//execution
+//buttons 'next' & 'prev' in main-slider
+btnNext.addEventListener("click", () => {
+    switchesToNextSlide();
 });
+btnPrev.addEventListener("click", () => {
+    switchesToPrevSlide();
+});
+
+//activation of switching slides by time
+switchesSlidesByTime();
