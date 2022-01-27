3b7c3a55e935cdd3b2696a78dc19e2963883df27
Yura Dubinin <yuradubinin100@gmail.com>
2022-01-27 20:59:11 +0700

    Minor edits
    * JS: 'use strict', added event listener ('DOMContentLoaded')
    
    * Added adaptive to adv block (video-item)
    
    * Added adaptive to main-slider

--- js/script.js
+++ js/script.js
@@ -1,100 +1,98 @@
-$(document).ready(function () {
-    $(".header__burger").click(function (event) {
-        $(".header__burger, .header__navigation").toggleClass("active");
-        $("body").toggleClass("scroll-lock");
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    $(document).ready(function () {
+        $(".header__burger").click(function (event) {
+            $(".header__burger, .header__navigation").toggleClass("active");
+            $("body").toggleClass("scroll-lock");
+        });
     });
-});
 
-/* main-slider */
-//declaration
-let slides = document.querySelectorAll(".main-slider__item"),
-    btnNext = document.querySelector(".main-slider-btns__next"),
-    btnPrev = document.querySelector(".main-slider-btns__prev");
+    /* main-slider */
+    //declaration
+    let slides = document.querySelectorAll(".main-slider__item"),
+        btnNext = document.querySelector(".main-slider-btns__next"),
+        btnPrev = document.querySelector(".main-slider-btns__prev");
 
-let slideIndex = 0;
+    let slideIndex = 0;
 
-//functions
-//next slide
-function switchesToNextSlide() {
-    if (slideIndex === slides.length - 1) {
-        slides[slideIndex].classList.remove("show", "fade");
-        slides[slideIndex].classList.add("hide");
-        slideIndex = 0;
-        slides[slideIndex].classList.remove("hide");
-        slides[slideIndex].classList.add("show", "fade");
-    } else {
-        slides[slideIndex].classList.remove("show", "fade");
-        slides[slideIndex].classList.add("hide");
-        ++slideIndex;
-        slides[slideIndex].classList.remove("hide");
-        slides[slideIndex].classList.add("show", "fade");
+    function switchesToNextSlide() {
+        if (slideIndex === slides.length - 1) {
+            slides[slideIndex].classList.remove("show", "fade");
+            slides[slideIndex].classList.add("hide");
+            slideIndex = 0;
+            slides[slideIndex].classList.remove("hide");
+            slides[slideIndex].classList.add("show", "fade");
+        } else {
+            slides[slideIndex].classList.remove("show", "fade");
+            slides[slideIndex].classList.add("hide");
+            ++slideIndex;
+            slides[slideIndex].classList.remove("hide");
+            slides[slideIndex].classList.add("show", "fade");
+        }
     }
-}
 
-//prev slide
-function switchesToPrevSlide() {
-    if (slideIndex === 0) {
-        slides[slideIndex].classList.remove("show", "fade");
-        slides[slideIndex].classList.add("hide");
-        slideIndex = slides.length - 1;
-        slides[slideIndex].classList.remove("hide");
-        slides[slideIndex].classList.add("show", "fade");
-    } else {
-        slides[slideIndex].classList.remove("show", "fade");
-        slides[slideIndex].classList.add("hide");
-        --slideIndex;
-        slides[slideIndex].classList.remove("hide");
-        slides[slideIndex].classList.add("show", "fade");
+    function switchesToPrevSlide() {
+        if (slideIndex === 0) {
+            slides[slideIndex].classList.remove("show", "fade");
+            slides[slideIndex].classList.add("hide");
+            slideIndex = slides.length - 1;
+            slides[slideIndex].classList.remove("hide");
+            slides[slideIndex].classList.add("show", "fade");
+        } else {
+            slides[slideIndex].classList.remove("show", "fade");
+            slides[slideIndex].classList.add("hide");
+            --slideIndex;
+            slides[slideIndex].classList.remove("hide");
+            slides[slideIndex].classList.add("show", "fade");
+        }
     }
-}
 
-//change slides by time
-function switchesSlidesByTime() {
-    /* switches slides in time, stops switching when the user hovers the 
-    mouse over the image or button, restores timer when leaving area */
-    let timerId = setInterval(switchesToNextSlide, 6000);
+    //change slides by time, needs to be reworked
+    function switchesSlidesByTime() {
+        /* switches slides in time, stops switching when the user hovers the 
+        mouse over the image or button, restores timer when leaving area */
+        let timerId = setInterval(switchesToNextSlide, 6000);
 
-    const slider = document.querySelector(".main-slider__slides");
+        const slider = document.querySelector(".main-slider__slides");
 
-    let images = slider.querySelectorAll("img");
+        let images = slider.querySelectorAll("img");
 
-    //stops automatic change of slides when howering over the image
-    images.forEach((item) => {
-        item.addEventListener("mouseenter", () => {
-            clearInterval(timerId);
+        //stops automatic change of slides when howering over the image
+        images.forEach((item) => {
+            item.addEventListener("mouseenter", () => {
+                clearInterval(timerId);
+            });
         });
-    });
 
-    //starts automatic change of slides when the mouse leaving from the image
-    images.forEach((item) => {
-        item.addEventListener("mouseleave", () => {
-            timerId = setInterval(switchesToNextSlide, 4000);
+        //starts automatic change of slides when the mouse leaving from the image
+        images.forEach((item) => {
+            item.addEventListener("mouseleave", () => {
+                timerId = setInterval(switchesToNextSlide, 4000);
+            });
         });
-    });
 
-    //stops automatic change of slides when howering over the slider buttons
-    [btnNext, btnPrev].forEach((item) => {
-        item.addEventListener("mouseenter", () => {
-            clearInterval(timerId);
+        //stops automatic change of slides when howering over the slider buttons
+        [btnNext, btnPrev].forEach((item) => {
+            item.addEventListener("mouseenter", () => {
+                clearInterval(timerId);
+            });
         });
-    });
 
-    //starts automatic change of slides when the mouse leaving from slider buttons
-    [btnNext, btnPrev].forEach((item) => {
-        item.addEventListener("mouseleave", () => {
-            timerId = setInterval(switchesToNextSlide, 4000);
+        //starts automatic change of slides when the mouse leaving from slider buttons
+        [btnNext, btnPrev].forEach((item) => {
+            item.addEventListener("mouseleave", () => {
+                timerId = setInterval(switchesToNextSlide, 4000);
+            });
         });
-    });
-}
+    }
 
-//execution
-//buttons 'next' & 'prev' in main-slider
-btnNext.addEventListener("click", () => {
-    switchesToNextSlide();
+    //execution
+    //buttons 'next' & 'prev' in main-slider
+    btnNext.addEventListener("click", () => {
+        switchesToNextSlide();
+    });
+    btnPrev.addEventListener("click", () => {
+        switchesToPrevSlide();
+    });
 });
-btnPrev.addEventListener("click", () => {
-    switchesToPrevSlide();
-});
-
-//activation of switching slides by time
-switchesSlidesByTime();
