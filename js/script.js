5a60d26460757119c4159487605513b4ec73a2ed
Yura Dubinin <yuradubinin100@gmail.com>
2022-02-17 15:11:21 +0700

    Working with npm packeges & added JSON-server
    * Init npm project, added 'package-lock.json' & 'package.json'
    
    * Installed npm package 'json-server', added 'db.json'

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 "use strict";
 
-// Burger-menu
 document.addEventListener("DOMContentLoaded", () => {
+    //Burger-menu
     document.querySelector(".header__burger").addEventListener("click", () => {
         document.querySelector(".header__burger").classList.toggle("active");
         document.querySelector(".header__navigation").classList.toggle("active");
         document.querySelector("body").classList.toggle("scroll-lock");
     });
+
+    //test db
+    fetch("http://localhost:3000/menu")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+        });
 });
